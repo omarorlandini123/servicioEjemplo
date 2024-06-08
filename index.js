01b1b1539d823cd546a2aa9f7b762a566457df5e
Omar Orlandini <omarorlandini123@gmail.com>
2024-06-08 18:32:37 -0500

    Nuevo objeto

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const hostname = '0.0.0.0';
 const port = 80;
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola mund@');
+  res.setHeader('Content-Type', 'text/json');
+
+  let arreglo = [
+    {
+      nombre:"Juan",
+      apellido:"Perez"
+    },
+    {
+      nombre:"Mario",
+      apellido:"Bross"
+    },
+    {
+      nombre: "Juan",
+      apellido: "Flores"
+    }
+  ]
+
+  res.write(JSON.stringify(arreglo));
+
+  res.end('');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
